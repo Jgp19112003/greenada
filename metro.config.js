880f64a5099a83cc19e07347fe28cb9db8dd5e94
Jorge González <jgp19112003@gmail.com>
2025-05-12 23:46:05 +0200

    fix: migrating development build
    Se migra a development build ya que Expo Go es necesario SDK 53 y da erro al  hacer update de todo el proyecto

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,14 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Explicitly handle resolver configuration
+config.resolver = {
+  ...config.resolver,
+  assetExts: [
+    ...config.resolver.assetExts.filter((ext) => ext !== "ttf"),
+    "ttf",
+  ], // Ensure 'ttf' is included
+  sourceExts: [...config.resolver.sourceExts], // Use default sourceExts
+};
+
 module.exports = withNativeWind(config, { input: "./global.css" });
